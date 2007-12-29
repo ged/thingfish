445869114d42bad7255d09e28dc41e43fa365a8a
Michael Granger <ged@FaerieMUD.org>
2007-12-29 18:21:06 +0000

    Adding some more manual content for Handlers.

--- docs/manual/src/04.Hackers_Guide/examples/minimal-handler.rb
+++ docs/manual/src/04.Hackers_Guide/examples/minimal-handler.rb
@@ -8,6 +8,7 @@ class TimeHandler < ThingFish::Handler
 	### Send the local time to the requester in plain text.
 	def handle_get_request( request, response )
 		response.body   = Time.now.to_s
+		response.headers[:content_type] = 'text/plain'
 		response.status = HTTP::OK
 	end
 end

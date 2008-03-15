619da5060b32d64ab6e8259aa4cde7e892b4a545
Mahlon E. Smith <mahlon@martini.nu>
2008-03-15 00:28:20 +0000

    * Rename default sqlite3 file from 'metadata' to 'metastore'.  * Initial commit of the Sequel metastore plugin.    This is a non-functional checkpoint commit.  Not ready for prime time... but close.  ;)

---
+++ plugins/thingfish-metastore-sequel/spec/thingfish/metastore/sequel_spec.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+BEGIN {
+	require 'pathname'
+	plugindir = Pathname.new( __FILE__ ).dirname.parent.parent.parent
+	basedir = plugindir.parent.parent
+
+	libdir = basedir + "lib"
+	pluglibdir = plugindir + "lib"
+
+	$LOAD_PATH.unshift( libdir ) unless $LOAD_PATH.include?( libdir )
+	$LOAD_PATH.unshift( pluglibdir ) unless $LOAD_PATH.include?( pluglibdir )
+}
+
+begin
+	require 'pathname'
+	require 'tmpdir'
+	require 'spec/runner'
+	require 'spec/lib/metastore_behavior'
+	require 'spec/lib/constants'
+	require 'spec/lib/helpers'
+	require 'thingfish/metastore/sequel'
+rescue LoadError
+	unless Object.const_defined?( :Gem )
+		require 'rubygems'
+		retry
+	end
+	raise
+end
+
+include ThingFish::SpecHelpers
+include ThingFish::TestConstants
+
+#####################################################################
+###	C O N T E X T S
+#####################################################################
+
+describe ThingFish::SequelMetaStore do
+	
+	before(:all) do
+		setup_logging( :fatal )
+
+	    @store = ThingFish::MetaStore.create( 'sequel' )
+#		@store.begin_transaction
+	end
+
+	before(:each) do
+		pending "Yeah, I'm workin' on it"
+	end
+	
+	after(:each) do
+		@store.clear
+	end
+
+	after(:all) do
+#		@store.commit
+		reset_logging()
+	end
+
+	it_should_behave_like "A MetaStore"
+end
+
+# vim: set nosta noet ts=4 sw=4:

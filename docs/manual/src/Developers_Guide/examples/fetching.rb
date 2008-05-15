2a107b05ff5a2fef8259ab9493f266c6276db5d4
Mahlon E. Smith <mahlon@martini.nu>
2008-05-15 21:39:37 +0000

    * Update links under the manual, use new generation method.

--- docs/manual/src/Developers_Guide/examples/fetching.rb
+++
@@ -1,9 +0,0 @@
-require 'thingfish/client'
-
-client = ThingFish::Client.new( 'thingfish.laika.com' )
-resource = client.fetch( 'a3f4e060-dffa-11db-a8e7-4bd9759f4985' )
-
-resource.uuid								# => 
-resource.format								# => 
-resource.data[0..20]						# => 
-
--- docs/manual/src/Developers_Guide/examples/storing.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'thingfish/client'
-
-client = ThingFish::Client.new( 'thingfish.laika.com' )
-resource = client.store( File.open("rss.png"), :format => 'image/png' )
-
-resource.uuid # => "a52ec552-2c50-11dc-8e32-abc548881c84"
-

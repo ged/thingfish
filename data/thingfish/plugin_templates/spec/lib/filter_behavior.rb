421b8d0bab58cf6d43498eb01ef740ed78634583
Michael Granger <ged@FaerieMUD.org>
2009-10-14 06:24:07 -0700

    Rearranged the tf_plugin tool parts to function from an installed library or gem.  * Updated the build system.  * Moved plugin templates from plugins/.skel to data/thingfish/plugin_templates.  * Cleaned up and updated the templates  * Copied the shared behaviors and spec helpers into the template directory -- we    should probably just move at least the shared behaviors into the lib/thingfish/    directory so they can be loaded from an installed ThingFish gem/library.
    --HG--
    rename : plugins/.skel/LICENSE.erb => data/thingfish/plugin_templates/LICENSE.erb
    rename : plugins/.skel/README.erb => data/thingfish/plugin_templates/README.erb
    rename : plugins/.skel/Rakefile.erb => data/thingfish/plugin_templates/Rakefile.erb
    rename : plugins/.skel/lib/thingfish/filestore/TEMPLATE.rb.erb => data/thingfish/plugin_templates/lib/thingfish/filestore/TEMPLATE.rb.erb
    rename : plugins/.skel/lib/thingfish/filter/TEMPLATE.rb.erb => data/thingfish/plugin_templates/lib/thingfish/filter/TEMPLATE.rb.erb
    rename : plugins/.skel/lib/thingfish/handler/TEMPLATE.rb.erb => data/thingfish/plugin_templates/lib/thingfish/handler/TEMPLATE.rb.erb
    rename : plugins/.skel/lib/thingfish/metastore/TEMPLATE.rb.erb => data/thingfish/plugin_templates/lib/thingfish/metastore/TEMPLATE.rb.erb
    rename : plugins/.skel/spec/thingfish/filestore/TEMPLATE_spec.rb.erb => data/thingfish/plugin_templates/spec/thingfish/filestore/TEMPLATE_spec.rb.erb
    rename : plugins/.skel/spec/thingfish/filter/TEMPLATE_spec.rb.erb => data/thingfish/plugin_templates/spec/thingfish/filter/TEMPLATE_spec.rb.erb
    rename : plugins/.skel/spec/thingfish/handler/TEMPLATE_spec.rb.erb => data/thingfish/plugin_templates/spec/thingfish/handler/TEMPLATE_spec.rb.erb
    rename : plugins/.skel/spec/thingfish/metastore/TEMPLATE_spec.rb.erb => data/thingfish/plugin_templates/spec/thingfish/metastore/TEMPLATE_spec.rb.erb

---
+++ data/thingfish/plugin_templates/spec/lib/filter_behavior.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+BEGIN {
+	require 'pathname'
+	basedir = Pathname.new( __FILE__ ).dirname.parent
+	
+	libdir = basedir + "lib"
+	
+	$LOAD_PATH.unshift( libdir ) unless $LOAD_PATH.include?( libdir )
+}
+
+begin
+	require 'spec'
+	require 'spec/lib/constants'
+	require 'spec/lib/helpers'
+	require 'thingfish'
+	require 'thingfish/constants'
+	require 'thingfish/filter'
+rescue LoadError
+	unless Object.const_defined?( :Gem )
+		require 'rubygems'
+		retry
+	end
+	raise
+end
+
+
+describe "A Filter", :shared => true do
+	include ThingFish::TestConstants
+	include ThingFish::SpecHelpers
+
+
+	it "knows what types it handles" do
+		@filter.handled_types.should be_an_instance_of( Array )
+		@filter.handled_types.each {|f| f.should be_an_instance_of(ThingFish::AcceptParam) }
+	end
+
+	it "provides information about itself through its introspection interface" do
+		@filter.info.should be_an_instance_of( Hash )
+		@filter.info.should have_key( 'version' )
+		@filter.info['version'].should be_an_instance_of( Array )
+		@filter.info.should have_key( 'accepts' )
+		@filter.info['accepts'].should be_an_instance_of( Array )
+		@filter.info.should have_key( 'generates' )
+		@filter.info['generates'].should be_an_instance_of( Array )
+		@filter.info.should have_key( 'supports' )
+		@filter.info['supports'].should be_an_instance_of( Array )
+		@filter.info.should have_key( 'rev' )
+		@filter.info['rev'].to_s.should =~ /^\d+$/
+	end
+
+
+	it "knows what its plugin name is" do
+		@filter.class.plugin_name.should be_an_instance_of( String )
+	end
+	
+	
+end
+
---
+++ data/thingfish/plugin_templates/spec/lib/handler_behavior.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+BEGIN {
+	require 'pathname'
+	basedir = Pathname.new( __FILE__ ).dirname.parent.parent
+	
+	libdir = basedir + "lib"
+	
+	$LOAD_PATH.unshift( libdir ) unless $LOAD_PATH.include?( libdir )
+}
+
+begin
+	require 'spec'
+	require 'spec/lib/constants'
+	require 'spec/lib/helpers'
+	require 'thingfish'
+	require 'thingfish/constants'
+	require 'thingfish/handler'
+rescue LoadError
+	unless Object.const_defined?( :Gem )
+		require 'rubygems'
+		retry
+	end
+	raise
+end
+
+
+describe "A Handler", :shared => true do
+	include ThingFish::TestConstants
+	include ThingFish::SpecHelpers
+
+
+	after( :all ) do
+		ThingFish.reset_logger
+	end
+
+end
+

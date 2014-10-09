e837cd998651c6ddbb63fb7b2ba41917dac5166a
Mahlon E. Smith <mahlon@martini.nu>
2014-10-09 09:06:12 -0700

    Clean out historical artifacts, mostly pieces from the pre-strelka conversion.
     - Rename the plugins directory, these should be migrated to individual
       gems and then removed.
    
     - Remove the manual -- we'll likely base a new version on this one, but
       it is currently inaccurate and misleading, so lets resurrect (with
       accuracy!) later.
    
     - Move current notes under the experiments directory.
    
     - Remove old clients libraries, they too can be ressurected when the
       new handler environment is complete.

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 # Foreman procfile
 mongrel: m2sh.rb -c etc/mongrel2.sqlite start
-thingfish: ruby -Ilib bin/thingfishd etc/thingfish.conf
+thingfish: ruby -Ilib bin/thingfish etc/thingfish.conf
 
---
+++ etc/mongrel2-config.rb
@@ -0,0 +1,55 @@
+# -*- ruby -*-
+# vim: set nosta noet ts=4 sw=4:
+#encoding: utf-8
+
+#
+# This script generates a Mongrel2 configuration database suitable for
+# getting a Thingfish handler running.
+#
+# Load it with:
+#
+#     $ m2sh.rb -c mongrel2.sqlite load examples/mongrel2-config.rb
+#
+# Afterwards, ensure the path to the mongrel2.sqlite file is in your
+# thingfish.conf:
+#
+#     mongrel2:
+#       configdb: /path/to/mongrel2.sqlite
+#
+# ... and start the mongrel2 daemon:
+#
+#     $ mongrel2 /path/to/mongrel2.sqlite thingfish
+#
+# In production use, you'll likely want to mount the Thingfish handler
+# within the URI space of an existing Mongrel2 environment.
+#
+
+require 'mongrel2'
+require 'mongrel2/config/dsl'
+
+server 'thingfish' do
+	name         'Thingfish'
+	default_host 'localhost'
+
+	access_log   'logs/access.log'
+	error_log    'logs/error.log'
+	chroot       ''
+	pid_file     'run/mongrel2.pid'
+
+	bind_addr    '0.0.0.0'
+	port         3474
+
+	host 'localhost' do
+		route '/', handler( 'tcp://127.0.0.1:9900', 'thingfish' )
+	end
+end
+
+setting 'zeromq.threads', 1
+setting 'limits.content_length', 100 * 1024 * 1024
+setting 'server.daemonize', false
+setting 'upload.temp_store', 'var/uploads/mongrel2.upload.XXXXXX'
+
+mkdir_p 'var/uploads'
+mkdir_p 'run'
+mkdir_p 'logs'
+

b744a8f1b8716a39e90b4fa1cc34246586b884dc
Mahlon E. Smith <mahlon@martini.nu>
2016-11-13 15:36:18 -0800

    Add checksums to related resources as they are created.

--- spec/thingfish/processor/sha256_spec.rb
+++ spec/thingfish/processor/sha256_spec.rb
@@ -26,10 +26,23 @@ describe Thingfish::Processor, "SHA256" do
 
 	it "generates a sha256 checksum from an uploaded file" do
 		req = factory.post( '/tf', fixture_data('APIC-1-image.mp3'), 'Content-type' => 'audio/mp3' )
-
 		processor.process_request( req )
 
 		expect( req.metadata['checksum'] ).to eq( 'e6b7070cbec90cdc2d8206819d86d100f076f480c9ae19d3eb8f878b3b86f2d6' )
+	end
+
+
+	it "generates a sha256 checksum for related resources" do
+		content = "data data data data data"
+		req = factory.post( '/tf', fixture_data('APIC-1-image.mp3'), 'Content-type' => 'audio/mp3' )
+		req.add_related_resource( StringIO.new( content ), {} )
+
+		processor.process_request( req )
+
+		related = req.related_resources
+		related_metadata = related.first.last
+
+		expect( related_metadata[:checksum] ).to eq( Digest::SHA256.hexdigest(content) )
 	end
 end
 
